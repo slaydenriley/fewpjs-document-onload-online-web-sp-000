6a6c310f264b3f8ad5c2b64ca162f9c87c63dbc4
Riley Slayden <slaydenriley@gmail.com>
2020-05-04 15:10:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
-});
+document.addEventListener( "DOMContentLoaded", function () {
+  updateDOM()
+} );
 
-console.log(
-  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-);
+
+function updateDOM() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
+}
